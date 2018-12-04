87d0579fadceca6b8a8435d78c9f27b757bdc1ca
informathix <gconnan@uco.fr>
2018-12-04 11:10:29 +0100

    Documentation du code et amélioration de l'aspect de MaBase_MIB.py

--- TP_ALIENS_UCO/MIB.py
+++ TP_ALIENS_UCO/MIB.py
@@ -1,28 +1,58 @@
 # -*- coding: utf-8 -*-
 
-import glob
-# The glob module finds all the pathnames matching a specified pattern according to the rules used by the Unix shell
-from pathlib import Path
-# The final path component, without its suffix
+"""
+Fichier permettant de créer, à partir de fichiers csv, des tables sous formes d'ensembles
+contenant des objets créés sous forme de dataclass (i.e. ayant la structure d'enregistrement)
+"""
 
+import glob
+"""
+Le module glob permet de trouver tous les chemins en filtrant à l'aide d'expressions régulières
+"""
+from pathlib import Path
+"""
+Permet d'obtenir le nom du fichier sans l'extension
+"""
 
 mon_chemin = input('Quel est le chemin relatif du répertoire contenant les fichiers csv ?\n')
-#"./MIB_Files/"
+# eg ./MIB_Files/
 
 mon_alias = input('Alias du fichier py créé (sera ./MaBase_alias.py) ?\n')
+# eg MIB
 
 mon_fic = "MaBase_%s.py" % mon_alias
  
 mes_csv_file = {Path(f).stem:open(f,"r") for f in glob.glob(mon_chemin + "*.csv")}
+"""
+Les fichiers csv encapsulés 
+"""
 
 mes_csv = {Path(f).stem:open(f,"r").readlines() for f in glob.glob(mon_chemin + "*.csv")}
+"""
+Les fichiers csv sous forme de listes de chaque ligne donnée sous forme de chaîne de caractères
+"""
+
 
 mon_py = open(mon_fic,"w+")
+"""
+mon_py est le fichier python construit qui contiendra les tables
+"""
 
-mon_py.write("from dataclasses import dataclass\n\n")
-
+mon_py.write('# -*- coding: utf-8 -*-\n"""\nModule généré automatiquement par le fichier MIB.py\nIl crée des tables sous forme d\'ensembles contenant des objets\ncréés sous forme de dataclass (i.e. ayant la structure d\'enregistrement)\n"""\n\nfrom dataclasses import dataclass\n\n')
+"""
+Crée l'en-tête du fichier python
+"""
 
 def creer_classes():
+    """
+    Crée les dataclasses avec la liste des attributs et leur type (str)
+    Par exemple:
+
+    @dataclass(frozen=True)
+    class Cabine:
+	NoCabine: str
+	NoAllee: str
+    """
     for b in mes_csv:
         mon_py.write("@dataclass(frozen=True)\nclass " + b[4:-1] + ":\n\t")
         lignes = mes_csv[b]
@@ -33,26 +63,42 @@ def creer_classes():
         
             
 def creer_bases():
+    """
+    Crée lees tables sous forme d'ensemble des éléments de chaque dataclass créée
+    Par exemple:
+
+    BaseResponsables = {
+	Responsable('1', 'Seldon'),
+	Responsable('2', 'Pelorat') 
+    }
+    """
     for b in mes_csv:
         nom = b[4:-1]
-        mon_py.write(b + " = { ")
+        mon_py.write(b + " = {\n\t")
         lignes = mes_csv[b]
         for index,ligne in enumerate(lignes[1:]):
             ligne = ligne.split()[0].split(',')
-            debut = '' if index == 0 else ', '
+            debut = '' if index == 0 else ',\n\t'
             mon_py.write(debut + nom + "(")
             for att in ligne[:-1]:
                 mon_py.write("'" + att + "', ")
             mon_py.write("'" + ligne[-1] +"')")
-        mon_py.write(" }\n\n")
+        mon_py.write(" \n}\n\n")
         
 
 def ferme():
+    """
+    Ferme les fichiers csv encapsulés
+    """
     for b in mes_csv_file:
         mes_csv_file[b].close()
     mon_py.close()
 
 
-creer_classes()
-creer_bases()
-ferme()
+if __name__ == '__main__':
+    """
+    Lance les différentes fonctions créées 
+    """
+    creer_classes()
+    creer_bases()
+    ferme()
--- TP_ALIENS_UCO/MaBase_MIB.py
+++ TP_ALIENS_UCO/MaBase_MIB.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Module généré automatiquement par le fichier MIB.py
+Il crée des tables sous forme d'ensembles contenant des objets
+créés sous forme de dataclass (i.e. ayant la structure d'enregistrement)
+"""
+
 from dataclasses import dataclass
 
 @dataclass(frozen=True)
@@ -31,16 +38,69 @@ class Alien:
 class Agent:
 	Nom: str
 	Ville: str
+	
+BaseCabines = {
+	Cabine('1', '1'),
+	Cabine('2', '1'),
+	Cabine('3', '1'),
+	Cabine('4', '1'),
+	Cabine('5', '1'),
+	Cabine('6', '2'),
+	Cabine('7', '2'),
+	Cabine('8', '2'),
+	Cabine('9', '2') 
+}
 
-BaseCabines = { Cabine('1', '1'), Cabine('2', '1'), Cabine('3', '1'), Cabine('4', '1'), Cabine('5', '1'), Cabine('6', '2'), Cabine('7', '2'), Cabine('8', '2'), Cabine('9', '2') }
+BaseGardiens = {
+	Gardien('Branno', '1'),
+	Gardien('Darell', '2'),
+	Gardien('Demerzel', '3'),
+	Gardien('Seldon', '4'),
+	Gardien('Dornick', '5'),
+	Gardien('Hardin', '6'),
+	Gardien('Trevize', '7'),
+	Gardien('Pelorat', '8'),
+	Gardien('Riose', '9') 
+}
 
-BaseGardiens = { Gardien('Branno', '1'), Gardien('Darell', '2'), Gardien('Demerzel', '3'), Gardien('Seldon', '4'), Gardien('Dornick', '5'), Gardien('Hardin', '6'), Gardien('Trevize', '7'), Gardien('Pelorat', '8'), Gardien('Riose', '9') }
+BaseResponsables = {
+	Responsable('1', 'Seldon'),
+	Responsable('2', 'Pelorat') 
+}
 
-BaseResponsables = { Responsable('1', 'Seldon'), Responsable('2', 'Pelorat') }
+BaseMiams = {
+	Miam('Zorglub', 'Bortsch'),
+	Miam('Blorx', 'Bortsch'),
+	Miam('Urxiz', 'Zoumise'),
+	Miam('Zbleurdite', 'Bortsch'),
+	Miam('Darneurane', 'Schwanstucke'),
+	Miam('Mulzo', 'Kashpir'),
+	Miam('Zzzzzz', 'Kashpir'),
+	Miam('Arghh', 'Zoumise'),
+	Miam('Joranum', 'Bortsch') 
+}
 
-BaseMiams = { Miam('Zorglub', 'Bortsch'), Miam('Blorx', 'Bortsch'), Miam('Urxiz', 'Zoumise'), Miam('Zbleurdite', 'Bortsch'), Miam('Darneurane', 'Schwanstucke'), Miam('Mulzo', 'Kashpir'), Miam('Zzzzzz', 'Kashpir'), Miam('Arghh', 'Zoumise'), Miam('Joranum', 'Bortsch') }
+BaseAliens = {
+	Alien('Zorglub', 'M', 'Trantor', '1'),
+	Alien('Blorx', 'M', 'Euterpe', '2'),
+	Alien('Urxiz', 'M', 'Aurora', '3'),
+	Alien('Zbleurdite', 'F', 'Trantor', '4'),
+	Alien('Darneurane', 'M', 'Trantor', '4'),
+	Alien('Mulzo', 'M', 'Helicon', '6'),
+	Alien('Zzzzzz', 'F', 'Aurora', '7'),
+	Alien('Arghh', 'M', 'Nexon', '8'),
+	Alien('Joranum', 'F', 'Euterpe', '9') 
+}
 
-BaseAliens = { Alien('Zorglub', 'M', 'Trantor', '1'), Alien('Blorx', 'M', 'Euterpe', '2'), Alien('Urxiz', 'M', 'Aurora', '3'), Alien('Zbleurdite', 'F', 'Trantor', '4'), Alien('Darneurane', 'M', 'Trantor', '4'), Alien('Mulzo', 'M', 'Helicon', '6'), Alien('Zzzzzz', 'F', 'Aurora', '7'), Alien('Arghh', 'M', 'Nexon', '8'), Alien('Joranum', 'F', 'Euterpe', '9') }
-
-BaseAgents = { Agent('Branno', 'Terminus'), Agent('Darell', 'Terminus'), Agent('Demerzel', 'Uco'), Agent('Seldon', 'Terminus'), Agent('Dornick', 'Kalgan'), Agent('Hardin', 'Terminus'), Agent('Trevize', 'Hesperos'), Agent('Pelorat', 'Kalgan'), Agent('Riose', 'Terminus') }
+BaseAgents = {
+	Agent('Branno', 'Terminus'),
+	Agent('Darell', 'Terminus'),
+	Agent('Demerzel', 'Uco'),
+	Agent('Seldon', 'Terminus'),
+	Agent('Dornick', 'Kalgan'),
+	Agent('Hardin', 'Terminus'),
+	Agent('Trevize', 'Hesperos'),
+	Agent('Pelorat', 'Kalgan'),
+	Agent('Riose', 'Terminus') 
+}
 
